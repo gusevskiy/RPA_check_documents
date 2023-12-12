0b56e43d6633534fac5ac424a3db62b52b7b0b52
gusevskiy <vlgu@mail.ru>
2023-12-12 13:53:07 +0300

    Первый тестовый вариант для сервера

--- aiogram-bot/io_main.py
+++ aiogram-bot/io_main.py
@@ -1,13 +1,14 @@
 import asyncio
 import os
 import logging
-from aiogram import Bot, Dispatcher, F, types, Router
-from aiogram.types import ContentType
+from aiogram import Bot, Dispatcher, F, types
 from aiogram.filters.command import Command
 from dotenv import load_dotenv
 
 
 load_dotenv()
+
+
 TOKEN = os.getenv('TOKEN')
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -15,7 +16,7 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
 # Диспетчер
 dp = Dispatcher()
-
+pathfile=f"{os.path.dirname(os.path.abspath(__file__))}\\chat_files"
 
 @dp.message(F.document)
 async def save_file(message: types.Message, bot: Bot):
@@ -25,23 +26,22 @@ async def save_file(message: types.Message, bot: Bot):
         file = await bot.get_file(file_id)
         file_path = file.file_path
         name_file = message.document.file_name
-        # print(file_id)
-        # print(file)
-        # print(file_path)
-        destination=f"C:\\robots\\RPA_telegram\\chat_files\\{name_file}"
+        # нужно создать папку chat_files если ее нет
+        destination=f"{pathfile}\\{name_file}"
         await bot.download_file(file_path, destination=destination)
     else:
         await message.answer(text='Мне такое расширение файлов не нужно!')
+
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     kb = [
         [types.KeyboardButton(text="Кнопка 1")],
-        [types.KeyboardButton(text="Кнопка 2")]
+        [types.KeyboardButton(text="удалить файлы")]
     ]
     keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
     await message.answer(
-        f"Вы девушка должны загрузить мне два файла " "\n" # noqa: F541
+        f"Вы товарищ должны загрузить мне два файла " "\n" # noqa: F541
         f"один с разширением PDF, тот из которого ищем, " "\n"  # noqa: F541
         f"второй с расширением XLSX, в котором ищем!",  # noqa: F541
         reply_markup=keyboard
@@ -50,12 +50,22 @@ async def cmd_start(message: types.Message):
 
 @dp.message()
 async def send_message(message: types.Message):
-    await message.answer(
-        text='Не надо мне писать, \n'
-             'я сам тебе напишу если захочу!'
-    )
-
-
+    adm = [452054525, ]
+    if message.chat.id not in adm:
+        await bot.send_message(message.chat.id, 'Мой хозяин вас не знает, '
+                                                'мне запрещено с '
+                                                'вами разговаривать!!!')
+    elif message.text == 'удалить файлы':
+        
+        if os.path.exists(pathfile):
+            for filename in os.listdir(pathfile):
+                os.remove(pathfile+"\\"+filename)
+    else:
+        await message.answer(
+            text='Не надо мне писать, \n'
+                'я сам тебе напишу если захочу! \n'
+                'Два файла пришли мне на проверку и все!'
+        )
 
 
 # Запуск процесса поллинга новых апдейтов
@@ -64,21 +74,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
-
-
-# @dp.message()
-# async def save_file(message: types.Message, bot: Bot):
-#     file_id = message.document.file_id
-#     name_file = message.document.file_name
-#     file = bot.get_file(file_id)
-#     file_path = file.file_path
-#     downloaded_file = bot.download_file(file)
-#     print("Это print", downloaded_file)
-#     document = message.document
-#     # await bot.download(document)
-#     await bot.download_file(file_path, name_file)
-#     # destination=f"C:\\robots\\RPA_telegram\\chat_files\\{name_file}.pdf"
-#     # print(name_file, destination)
-#     # await bot.download(document, destination=destination)
