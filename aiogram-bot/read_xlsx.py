d4483263b3b5e4f5ee73309ca849b77b0c30b739
gusevskiy <vlgu@mail.ru>
2023-12-19 17:06:45 +0300

    Вывел всю раскраску xlsx, работает

--- aiogram-bot/read_xlsx.py
+++ aiogram-bot/read_xlsx.py
@@ -9,32 +9,63 @@ def color_xlsx_cell(list_matches_pdf_file, xlsx_file):
     wb = openpyxl.reader.excel.load_workbook(filename=xlsx_file)
     wb.active = 0
     sheet = wb.active
+    iter_list = []  # Для исключения уже заграшеных строк
     for match in list_matches_pdf_file:
+        # Разбиваем на группы каждый элемент из list_matches_pdf_file
+        # Группы =>  date_document, number_document, amount_invoice
         dict_match = re.search(reg_groups, match).groupdict()
-        for row in sheet.iter_rows():
-            print(row)
-            date = dict_match['date_document']
-            number = dict_match['number_document']
-            amount = dict_match['amount_invoice'].replace(' ', '').split(",")[0].split(",")[0]
-            print(amount, row[4].value)
-            all_true = number in str(row[3].value) and (int(amount) == row[4].value or int(amount) == row[5].value)
-            all_false = number in str(row[3].value) and (int(amount) != row[4].value or int(amount) != row[5].value)
-            d=1
-            # if all_true:
-            #     row[3].fill = PatternFill(fill_type='solid', start_color='00FF00')
-            #     row[4].fill = PatternFill(fill_type='solid', start_color='00FF00')
-            #     row[5].fill = PatternFill(fill_type='solid', start_color='00FF00')
-            # elif number in str(row[3].value):
-            #     row[3].fill = PatternFill(fill_type='solid', start_color='CD5C5C')
-            #     row[4].fill = PatternFill(fill_type='solid', start_color='CD5C5C')
-            #     row[5].fill = PatternFill(fill_type='solid', start_color='CD5C5C')
+        for row in sheet.iter_rows():  # итерируемся по кождой строке в xlsx_file
+            pdf_date = dict_match['date_document']  # Даты не используются
+            pdf_number = dict_match['number_document']
+            pdf_amount = dict_match['amount_invoice'].replace(' ', '').split(",")[0].split(",")[0]
+            xlsx_column_1 = row[1].value  # Значение колонка номера строки
+            xlsx_column_3 = row[3].value  # Значение колонка Документ
+            xlsx_column_4 = row[4].value  # Значение колонка Дебет
+            xlsx_column_5 = row[5].value  # Зничение колонка Кредит
+            if type(xlsx_column_1) is int and xlsx_column_1 not in iter_list:
+                if pdf_number in xlsx_column_3 and (int(pdf_amount) == xlsx_column_4 or int(pdf_amount) == xlsx_column_5):
+                    # print(xlsx_column_1, "Yes")
+                    iter_list.append(xlsx_column_1)
+                    row[3].fill = PatternFill(fill_type='solid', start_color='00FF00')
+                    row[4].fill = PatternFill(fill_type='solid', start_color='00FF00')
+                    row[5].fill = PatternFill(fill_type='solid', start_color='00FF00')
+                elif pdf_number in xlsx_column_3 and (int(pdf_amount) != xlsx_column_4 or int(pdf_amount) != xlsx_column_5):
+                    # print(xlsx_column_1, "No")
+                    iter_list.append(xlsx_column_1)
+                    row[3].fill = PatternFill(fill_type='solid', start_color='FF9999')
+                    row[4].fill = PatternFill(fill_type='solid', start_color='FF9999')
+                    row[5].fill = PatternFill(fill_type='solid', start_color='FF9999')
+                elif pdf_number not in xlsx_column_3:
+                    print(pdf_number)
+                break  # Выход из внутреннего цикла и переход к следующему match из list_matches_pdf_file
             
-    wb.save('result.xlsx')
+    for row in sheet.iter_rows():  # итерируемся по кождой строке в xlsx_file
+        xlsx_column_1 = row[1].value  # Значение колонка номера строки
+        xlsx_column_3 = row[3].fill  # Значение колонка Документ
+        if type(xlsx_column_1) is int and row[3].fill.fgColor.value == "00000000":
+            row[3].fill = PatternFill(fill_type='solid', start_color='FFFF99')
+            row[4].fill = PatternFill(fill_type='solid', start_color='FFFF99')
+            row[5].fill = PatternFill(fill_type='solid', start_color='FFFF99')
+    wb.save(xlsx_file)
     
+def check_color_cell(xlsx_file):
+    wb = openpyxl.reader.excel.load_workbook(filename=xlsx_file)
+    wb.active = 0
+    sheet = wb.active
+    for row in sheet.iter_rows():  # итерируемся по кождой строке в xlsx_file
+        xlsx_column_1 = row[1].value  # Значение колонка номера строки
+        xlsx_column_3 = row[3].fill  # Значение колонка Документ
+        if type(xlsx_column_1) is int and row[3].fill.fgColor.value == "00000000":
+            row[3].fill = PatternFill(fill_type='solid', start_color='FFFF99')
+            row[4].fill = PatternFill(fill_type='solid', start_color='FFFF99')
+            row[5].fill = PatternFill(fill_type='solid', start_color='FFFF99')
+    wb.save('result.xlsx')
 
 
 if __name__ == '__main__':
     list_matches_pdf_file = ['27.01.23\\nОплата (120 от 27.01.2023)\\n32 000,00', '31.01.23\\nПродажа (26 от 31.01.2023)\\n32 000,00', '28.02.23\\nПродажа (61 от 28.02.2023)\\n32 000,00', '01.03.23\\nОплата (283 от 01.03.2023)\\n32 000,00', '29.03.23\\nОплата (447 от 29.03.2023)\\n32 000,00', '31.03.23\\nПродажа (98 от 31.03.2023)\\n32 000,00', '30.04.23\\nПродажа (134 от 30.04.2023)\\n32 000,00', '10.05.23\\nОплата (627 от 10.05.2023)\\n32 000,00', '31.05.23\\nПродажа (171 от 31.05.2023)\\n32 000,00', '08.06.23\\nОплата (786 от 08.06.2023)\\n32 000,00', '30.06.23\\nПродажа (204 от 30.06.2023)\\n32 000,00', '11.07.23\\nОплата (971 от 11.07.2023)\\n32 000,00', '31.07.23\\nПродажа (243 от 31.07.2023)\\n32 000,00', '17.08.23\\nОплата (1130 от 17.08.2023)\\n32 000,00', '18.08.23\\nОплата (1136 от 18.08.2023)\\n32 000,00', '31.08.23\\nПродажа (277 от 31.08.2023)\\n32 000,00']
     xlsx_file = 'aiogram-bot\chat_files\акт эф решение.xlsx'
+    result_xlsx_file = 'result.xlsx'
     
     color_xlsx_cell(list_matches_pdf_file, xlsx_file)
+    # check_color_cell(result_xlsx_file)
