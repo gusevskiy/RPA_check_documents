5b4458197ec22605f736e672acc34745e83c897a
Gusevskiy <vlgu@mail.ru>
2024-01-05 15:49:03 +0300

    start new bot

--- aiogram-bot/io_main.py
+++ aiogram-bot/io_main.py
@@ -154,7 +154,7 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    # asyncio.run(main())
     # if create_path_files(pathfile):
     #     print('yes')
     # print('no')
--- correct_bot/main.py
+++ correct_bot/main.py
@@ -1,19 +1,34 @@
 from aiogram import Bot, Dispatcher
+from aiogram.types import Message
 import asyncio
 
 token = '6513692439:AAFFgGmpXQfJzFsEUSiEP3FFK5QIZNqimO4'
 
+async def start_bot(bot: Bot):
+    await bot.send_message(452054525, text="Bot started")
 
 
+async def stop_bot(bot: Bot):
+    await bot.send_message(452054525, text="Bot stopped")
+
+
+async def get_start(message: Message, bot: Bot):
+    await message.answer(
+        f"Привет {message.from_user.first_name}. Рад тебя видеть!"
+    )
 
 
 async def start():
     bot = Bot(token=token)
     dp = Dispatcher()
+    dp.startup.register(start_bot)
+    dp.shutdown.register(stop_bot)
+
+    dp.message.register(get_start)
     try:
-        await dp.start_polling()
+        await dp.start_polling(bot)
     finally:
-        await dp.stop_polling()
+        await bot.session.close()
 
 
 if __name__ == '__main__':
