58ccb6820d96164552072c06b23c61d0847bb072
Gusevskiy <vlgu@mail.ru>
2024-02-16 23:47:43 +0300

    Файлы сохраняет, нужно решить проблему получения одного файла, что не было ложного срабатывания.

--- core/handlers/documents.py
+++ core/handlers/documents.py
@@ -7,26 +7,18 @@ from core.utils.statedocuments import StepsDocuments
 from core.settings import settings
 
 
-async def download(message: Message, bot: Bot, state: FSMContext):
+async def download(bot: Bot, state: FSMContext):
     data = await state.get_data()
     files_info = data.get('files_info', [])
-    print(files_info)
     for i in files_info:
         file_id = i["file_id"]
-        doc = await bot.get_file(message.document.file_id)
+        file_name = i["file_name"]
+        doc = await bot.get_file(file_id)
         await bot.download_file(
-            doc.file_id, f"{settings.bots.path_folder}\\{i["file_name"]}"
+            doc.file_path, f"{settings.bots.path_folder}\\{file_name}"
             # doc.file_path, destination=f"{pathfile}\\{name_doc}"
         )
-        # print()
 
-    # doc = await bot.get_file(message.document.file_id)
-    # name_doc = message.document.file_name
-    # # file_path = doc.file_path
-    # await bot.download_file(
-    #     doc.file_path, f"{settings.bots.path_folder}\\{name_doc}"
-    #     # doc.file_path, destination=f"{pathfile}\\{name_doc}"
-    # )
 
 async def req_document(message: Message, state: FSMContext):
     await message.reply(f"{message.from_user.first_name} Присылай файлы!")
@@ -34,17 +26,14 @@ async def req_document(message: Message, state: FSMContext):
 
 
 async def get_document(message: Message, bot: Bot, state: FSMContext):
-    resolved_extension = [".pdf", ".xlsx"]
-    recieved_extension = []
+    resolved_extension = [".pdf", ".xlsx"]  # Список разрешонных
+    recieved_extension = []  # Список полученых
     data = await state.get_data()  # Получение данных из состояния
     files_info = data.get('files_info', [])  # Получение информации о файлах или пустого списка, если информации еще нет
-    # file = await bot.get_file(message.document.file_id)
-    # file_path = file.file_path
     file_info = {
         'file_id': message.document.file_id,
         'file_name': message.document.file_name,
         'file_size': message.document.file_size,
-        # 'file_path': file_path
     }
     files_info.append(file_info)  # Добавление информации о файле в список файлов
     await state.update_data(files_info=files_info)  # Обновление данных в состоянии
@@ -55,15 +44,8 @@ async def get_document(message: Message, bot: Bot, state: FSMContext):
         recieved_extension.append(os.path.splitext(files_info[1]["file_name"])[1])
         if Counter(resolved_extension) == Counter(recieved_extension):
             await message.reply("Скачиваю файлы и проверяю.")
-            # await download(message, bot, state)
-            for i in files_info:
-                file_id = i["file_id"]
-                doc = await bot.get_file(file_id)
-                await bot.download_file(
-                    doc.file_path, f"{settings.bots.path_folder}\\{i["file_name"]}"
-                    # doc.file_path, destination=f"{pathfile}\\{name_doc}"
-                )
-            # print(data)
+            await download(bot, state)  # Скачиваем файлы с телеграмма на сервер.
+            await state.clear()  # Очищаем состояние
         else:
             await message.reply("Что то не то с расширениями файлов! Нажми /help")
 
