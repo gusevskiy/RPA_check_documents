5a51bb963dfe7ad0456c9953693889d5ac52f447
gusevskiy <vlgu@mail.ru>
2024-02-21 15:22:07 +0300

    Выпиливаю state похоже всетаки буду делать тупо через папку, с ее очисткой.

--- core/handlers/documents.py
+++ core/handlers/documents.py
@@ -17,33 +17,19 @@ async def req_document(message: Message, state: FSMContext):
 
 
 async def get_document(message: Message, bot: Bot, state: FSMContext):
-    name_file = message.document.file_name
-    if name_file.endswith(".pdf"):
-        file_pdf = {
-            'file_id': message.document.file_id,
-            'file_name': message.document.file_name,
-            'file_size': message.document.file_size,
-        }
-        await state.update_data(file_pdf=file_pdf)
-    elif name_file.endswith(".xlsx"):
-        file_xlsx = {
-            'file_id': message.document.file_id,
-            'file_name': message.document.file_name,
-            'file_size': message.document.file_size,
-        }
-        await state.update_data(file_xlsx=file_xlsx)
+    file_name = message.document.file_name
+    file_id = message.document.file_id
+    file_path = (await bot.get_file(file_id)).file_path
+    if file_name.endswith(".pdf"):
+        await download(file_path, file_name, bot)
+    elif file_name.endswith(".xlsx"):
+        await download(file_path, file_name, bot)
     else:
-        await message.reply(f"Такие файлы я не обрабытываю {name_file}")
+        await message.reply(f"Такие файлы я не обрабытываю {file_name}")
         return
-    await state.set_state(StepsDocuments.CHECK_DOCUMENT)
-    data = await state.get_data()
-    # print(data)
-    if len(data) == 2:
-        data = await state.get_data()
-        file_pdf = (await bot.get_file(data['file_pdf']["file_id"])).file_path
-        file_xlsx = (await bot.get_file(data['file_xlsx']["file_id"])).file_path
-        await bot.download(data.file_pdf, f"C:\\robots\\RPA_check_documents\\doc\\name.pdf")
-
+    print(data)
+    # if len(data) == 2:
+    #     await download(bot, state)
 
 
 async def open_stste(message: Message, state: FSMContext):
--- core/utils/downloads.py
+++ core/utils/downloads.py
@@ -4,14 +4,7 @@ from aiogram.fsm.context import FSMContext
 from core.settings import settings
 
 
-async def download(bot: Bot, state: FSMContext):
-    data = await state.get_data()
-    files_info = data.get('files_info', [])
-    for i in files_info:
-        file_id = i["file_id"]
-        file_name = i["file_name"]
-        doc = await bot.get_file(file_id)
-        await bot.download_file(
-            doc.file_path, f"{settings.bots.path_folder}\\{file_name}"
-            # doc.file_path, destination=f"{pathfile}\\{name_doc}"
-        )
+async def download(file_path, file_name, bot):
+    await bot.download_file(
+        file_path, f"{settings.bots.path_folder}\\{file_name}"
+    )
